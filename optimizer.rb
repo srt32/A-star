932c378b8f322228ebecd09f0a2bb60f3dfbe47d
Simon Taranto <simon.taranto@gmail.com>
2014-01-26 19:02:15 -0700

    WIP, optimizer infinitely looping over the space

--- optimizer.rb
+++ optimizer.rb
@@ -1,15 +1,46 @@
 class Optimizer
+  attr_reader :start_node,
+              :goal_node,
+              :matrix
 
   def initialize(matrix, start_node, goal_node)
-
+    @start_node = start_node
+    @goal_node = goal_node
+    @matrix = matrix
   end
 
   def least_cost_path
+    path = []
+    path << start_node
+    while start_node != goal_node do
+      next_node = best_next_node(start_node)
+      path << next_node
+      start_node = next_node
+    end
     [
-      [1,1],
+      start_node,
       [2,1],
-      [2,2]
+      goal_node
     ]
+    path
+  end
+
+  def best_next_node(current_position)
+    adjacent_nodes = adjacent_nodes_for(current_position)
+    return goal_node if adjacent_nodes.include?(goal_node)
+    next_best_node = adjacent_nodes.min_by do |node|
+      matrix.node_at(node[0],node[1]).resistance
+    end
+  end
+
+  def adjacent_nodes_for(current_position)
+    nodes = []
+    curr_x = current_position[0]
+    curr_y = current_position[1]
+    # want to add/subtract unless the node is at max/min
+    nodes << [curr_x + 1, curr_y]
+    nodes << [curr_x, curr_y + 1]
+    nodes << [curr_x + 1, curr_y + 1] # additions are duplicated, optimize
   end
 
 end
--- optimizer_test.rb
+++ optimizer_test.rb
@@ -13,8 +13,20 @@ class OptimizerTest < MiniTest::Test
     ]
   end
 
+  def three_by_three
+    [
+      [0,0,0],
+      [1,1,0],
+      [1,1,0]
+    ]
+  end
+
   def matrix
     Matrix.new(two_by_two)
+  end
+
+  def matrix_3
+    Matrix.new(three_by_three)
   end
 
   def test_it_returns_least_cost_path_for_2_by_2
@@ -25,6 +37,30 @@ class OptimizerTest < MiniTest::Test
              [2,2]
            ]
     assert_equal optimizer.least_cost_path, path
+  end
+
+  def test_it_returns_least_cost_path_for_3_by_3
+    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
+    path = [
+             [1,1],
+             [2,1],
+             [3,1],
+             [3,2],
+             [3,3]
+           ]
+    assert_equal optimizer.least_cost_path, path
+  end
+
+  def test_best_next_node_selects_least_available_resistance
+    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
+    next_nodes = optimizer.best_next_node([1,1])
+    assert_equal [2,1], next_nodes
+  end
+
+  def test_finds_adjacent_nodes
+    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
+    adjacent_nodes = optimizer.adjacent_nodes_for([1,1])
+    assert_equal [ [2,1], [2,2], [1,2] ].sort, adjacent_nodes.sort
   end
 
 end
