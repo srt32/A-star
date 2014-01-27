08ad78e4c7ad4cbbebb66761dc37e16a25a80396
Simon Taranto <simon.taranto@gmail.com>
2014-01-26 19:44:19 -0700

    get 3x3 to work with no backtrack and no overflow nodes

--- optimizer.rb
+++ optimizer.rb
@@ -1,34 +1,43 @@
+require 'pry'
+
 class Optimizer
   attr_reader :start_node,
               :goal_node,
               :matrix
 
+  attr_accessor :visited_nodes
+
   def initialize(matrix, start_node, goal_node)
     @start_node = start_node
     @goal_node = goal_node
     @matrix = matrix
+    @visited_nodes = []
   end
 
   def least_cost_path
     path = []
     path << start_node
-    while start_node != goal_node do
-      next_node = best_next_node(start_node)
+    visited_nodes << start_node
+    starting_node = @start_node
+    loop do
+      next_node = best_next_node(starting_node)
       path << next_node
-      start_node = next_node
+      starting_node = next_node
+      break if starting_node == goal_node
     end
-    [
-      start_node,
-      [2,1],
-      goal_node
-    ]
     path
   end
 
   def best_next_node(current_position)
     adjacent_nodes = adjacent_nodes_for(current_position)
+
     return goal_node if adjacent_nodes.include?(goal_node)
-    next_best_node = adjacent_nodes.min_by do |node|
+
+    available_adjacent_nodes = adjacent_nodes.reject do |node|
+      visited_nodes.include?(node)
+    end
+
+    next_best_node = available_adjacent_nodes.min_by do |node|
       matrix.node_at(node[0],node[1]).resistance
     end
   end
@@ -38,9 +47,17 @@ class Optimizer
     curr_x = current_position[0]
     curr_y = current_position[1]
     # want to add/subtract unless the node is at max/min
-    nodes << [curr_x + 1, curr_y]
-    nodes << [curr_x, curr_y + 1]
-    nodes << [curr_x + 1, curr_y + 1] # additions are duplicated, optimize
+    max_size = matrix.resistances.length # MOVE INTO MATRIX CLASS
+    unless curr_x == max_size
+      nodes << [curr_x + 1, curr_y]
+    end
+    unless curr_y == max_size
+      nodes << [curr_x, curr_y + 1]
+    end
+    unless curr_y == max_size || curr_x == max_size
+      nodes << [curr_x + 1, curr_y + 1]
+    end
+    nodes
   end
 
 end
--- optimizer_test.rb
+++ optimizer_test.rb
@@ -33,14 +33,13 @@ class OptimizerTest < MiniTest::Test
     optimizer = Optimizer.new(matrix, [1,1], [2,2])
     path = [
              [1,1],
-             [2,1],
              [2,2]
            ]
-    assert_equal optimizer.least_cost_path, path
+    assert_equal path, optimizer.least_cost_path
   end
 
   def test_it_returns_least_cost_path_for_3_by_3
-    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
+    optimizer = Optimizer.new(matrix_3, [1,1], [3,3])
     path = [
              [1,1],
              [2,1],
@@ -48,17 +47,22 @@ class OptimizerTest < MiniTest::Test
              [3,2],
              [3,3]
            ]
-    assert_equal optimizer.least_cost_path, path
+    assert_equal path, optimizer.least_cost_path
   end
 
   def test_best_next_node_selects_least_available_resistance
-    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
-    next_nodes = optimizer.best_next_node([1,1])
-    assert_equal [2,1], next_nodes
+    optimizer = Optimizer.new(matrix_3, [1,1], [3,3])
+    next_node = optimizer.best_next_node([1,1])
+    assert_equal [2,1], next_node
+    next_node = optimizer.best_next_node([2,1])
+    assert_equal [3,1], next_node
+    next_node = optimizer.best_next_node([3,1])
+    assert_equal [3,2], next_node
+
   end
 
   def test_finds_adjacent_nodes
-    optimizer = Optimizer.new(matrix_3, [1,1], [2,2])
+    optimizer = Optimizer.new(matrix_3, [1,1], [3,3])
     adjacent_nodes = optimizer.adjacent_nodes_for([1,1])
     assert_equal [ [2,1], [2,2], [1,2] ].sort, adjacent_nodes.sort
   end
